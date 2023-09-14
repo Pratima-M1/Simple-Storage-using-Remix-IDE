a2523aac684b6615ff57c23883c6cb7e4dd55486
Pratima-M1 <pratimamudenagudi@gmail.com>
2023-09-14 20:14:18 +0530

    initial commit

--- contracts/demo.sol
+++ contracts/demo.sol
@@ -5,7 +5,8 @@ contract PriceConsumerV3{
 
     AggregatorV3Interface internal  priceFeed;
     constructor(){
-        priceFeed=AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+       // https://docs.chain.link/data-feeds/price-feeds/addresses?network=ethereum&page=1#Goerli%20Testnet
+        priceFeed=AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);//goerli USD/ETH price feed address
     }
 
     function getLatestPrice() public view returns(int){
